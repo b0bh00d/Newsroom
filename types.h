ea42f8d4e099109de21481fa6db33fb71785dacc
Bob Hood <b0bh00d@users.noreply.github.com>
2016-11-28 11:38:33 -0700

    Added the X() macro trick for declaring enumerateds alongside string versions for the AnimEntryType and AnimExitType enums.

--- types.h
+++ types.h
@@ -3,36 +3,16 @@
 // Order is important here so they match the GUI controls
 enum AnimEntryType
 {
-    SlideDownLeftTop,
-    SlideDownCenterTop,
-    SlideDownRightTop,
-    SlideInLeftTop,
-    SlideInRightTop,
-    SlideInLeftBottom,
-    SlideInRightBottom,
-    SlideUpLeftBottom,
-    SlideUpRightBottom,
-    SlideUpCenterBottom,
-    FadeInCenter,
-    PopCenter,
-    PopLeftTop,
-    PopRightTop,
-    PopLeftBottom,
-    PopRightBottom
+#   define X(a) a,
+#   include "animentrytype.def"
+#   undef X
 };
 
 enum AnimExitType
 {
-    Pop,
-    Fade,
-    SlideLeft,
-    SlideRight,
-    SlideUp,
-    SlideDown,
-    SlideFadeLeft,
-    SlideFadeRight,
-    SlideFadeUp,
-    SlideFadeDown
+#   define X(a) a,
+#   include "animexittype.def"
+#   undef X
 };
 
 enum ReportStacking
