6e6daa540e8f97c8680cbca38a856746fd7c95eb
Bob Hood <b0bh00d@users.noreply.github.com>
2016-12-25 14:52:49 -0700

    - Migrated text scaling into the label paint() event instead of doing it in the Headline class.
    - Added a QHLabel class.
    
    - Added respect for alignment settings in all label classes.

--- qvlabel.cpp
+++ qvlabel.cpp
@@ -2,17 +2,16 @@
 
 #include <QtGui/QPainter>
 #include <QtGui/QTextDocument>
-#include <QtGui/QTextBlock>
 #include <QtGui/QAbstractTextDocumentLayout>
 
 QVLabel::QVLabel(QWidget *parent)
-    : QLabel(parent)
+    : NewsroomLabel(parent)
 {
 }
 
 QVLabel::QVLabel(const QString &text, bool configure_for_left, QWidget *parent)
     : configure_for_left(configure_for_left),
-      QLabel(text, parent)
+      NewsroomLabel(text, parent)
 {
 }
 
@@ -21,41 +20,64 @@ void QVLabel::paintEvent(QPaintEvent* /*event*/)
     QPainter painter(this);
 
     QRect s = geometry();
+
+    QTextDocument td;
+    td.setHtml(text());
+    QSizeF doc_size = td.documentLayout()->documentSize();
+
+    if(shrink_text_to_fit)
+    {
+        QFont f = td.defaultFont();
+        for(;;)
+        {
+            doc_size = td.documentLayout()->documentSize();
+            if(doc_size.width() < s.height())
+                break;
+            f.setPointSizeF(f.pointSizeF() - .1);
+            td.setDefaultFont(f);
+        }
+    }
+
+    int y = 0;
+    int x = 0;
+
+    if(alignment() & Qt::AlignVCenter)
+    {
+        y = (s.width() - doc_size.height()) / 2;
+        y = (y < 0) ? 0 : y;
+    }
+    else if(alignment() & Qt::AlignBottom)
+    {
+        y = s.width() - doc_size.height();
+        y = (y < 0) ? 0 : y;
+    }
+
+    if(alignment() & Qt::AlignHCenter)
+    {
+        x = (s.height() - doc_size.width()) / 2;
+        x = (x < 0) ? 0 : x;
+    }
+    else if(alignment() & Qt::AlignRight)
+    {
+        x = s.height() - doc_size.width();
+        x = (x < 0) ? 0 : x;
+    }
+
     if(configure_for_left)
     {
-        painter.translate(0, s.height());
+        painter.translate(y, s.height() - x);
         painter.rotate(270);
     }
     else
     {
-        painter.translate(s.width(), 0);
+        painter.translate(s.width() - y, x);
         painter.rotate(90);
     }
-
-    QTextDocument td;
-    td.setHtml(text());
 
     QAbstractTextDocumentLayout::PaintContext ctx;
     ctx.palette.setColor(QPalette::Text, painter.pen().color());
     ctx.clip = QRect(0, 0, s.width(), s.height());
     td.documentLayout()->draw( &painter, ctx );
-
-///    painter.drawText(QRect(0, 0, s.height(), s.width()), Qt::AlignHCenter|Qt::AlignVCenter, text());
-
-//    painter.save();
-//    painter.translate(sizeHint().width(),0);
-//    painter.rotate(90);
-//    painter.drawText(QRect(QPoint(0,0), QLabel::sizeHint()), Qt::AlignLeft, text());
-//    painter.restore();
-
-//    painter.translate(sizeHint().width(),0);
-//    painter.rotate(90);
-
-//    painter.translate(0, sizeHint().height());
-//    painter.rotate(270);
-
-//    QRect bounding = QRect(QPoint(0,0), QLabel::sizeHint());
-//    painter.drawText(bounding, Qt::AlignCenter, text(), &bounding);
 }
 
 QSize QVLabel::minimumSizeHint() const
@@ -65,6 +87,85 @@ QSize QVLabel::minimumSizeHint() const
 }
 
 QSize QVLabel::sizeHint() const
+{
+    QSize s = QLabel::sizeHint();
+    return QSize(s.width(), s.height());
+}
+
+
+QHLabel::QHLabel(QWidget *parent)
+    : NewsroomLabel(parent)
+{
+}
+
+QHLabel::QHLabel(const QString &text, QWidget *parent)
+    : NewsroomLabel(text, parent)
+{
+}
+
+void QHLabel::paintEvent(QPaintEvent* /*event*/)
+{
+    QPainter painter(this);
+
+    QRect s = geometry();
+
+    QTextDocument td;
+    td.setHtml(text());
+    QSizeF doc_size = td.documentLayout()->documentSize();
+
+    if(shrink_text_to_fit)
+    {
+        QFont f = td.defaultFont();
+        for(;;)
+        {
+            doc_size = td.documentLayout()->documentSize();
+            if(doc_size.width() < s.width())
+                break;
+            f.setPointSizeF(f.pointSizeF() - .1);
+            td.setDefaultFont(f);
+        }
+    }
+
+    int y = 0;
+    int x = 0;
+
+    if(alignment() & Qt::AlignVCenter)
+    {
+        y = (s.height() - doc_size.height()) / 2;
+        y = (y < 0) ? 0 : y;
+    }
+    else if(alignment() & Qt::AlignBottom)
+    {
+        y = s.height() - doc_size.height();
+        y = (y < 0) ? 0 : y;
+    }
+
+    if(alignment() & Qt::AlignHCenter)
+    {
+        x = (s.width() - doc_size.width()) / 2;
+        x = (x < 0) ? 0 : x;
+    }
+    else if(alignment() & Qt::AlignRight)
+    {
+        x = s.width() - doc_size.width();
+        x = (x < 0) ? 0 : x;
+    }
+
+    painter.translate(x, y);
+
+    QAbstractTextDocumentLayout::PaintContext ctx;
+    ctx.palette.setColor(QPalette::Text, painter.pen().color());
+    ctx.clip = QRect(0, 0, s.width(), s.height());
+    td.documentLayout()->draw(&painter, ctx);
+}
+
+QSize QHLabel::minimumSizeHint() const
+{
+    QSize s = QLabel::minimumSizeHint();
+    return QSize(s.width(), s.height());
+}
+
+QSize QHLabel::sizeHint() const
 {
     QSize s = QLabel::sizeHint();
     return QSize(s.width(), s.height());
--- qvlabel.h
+++ qvlabel.h
@@ -4,13 +4,30 @@
 
 #include <QtGui/QPaintEvent>
 
+class NewsroomLabel : public QLabel
+{
+    Q_OBJECT
+public:
+    explicit NewsroomLabel(QWidget *parent = 0)
+        : shrink_text_to_fit(false), QLabel(parent) {}
+    explicit NewsroomLabel(const QString &text, QWidget *parent=0)
+        : shrink_text_to_fit(false), QLabel(text, parent) {}
+
+    virtual void shrink_to_fit(bool shrink = true) = 0;
+
+protected:
+    bool    shrink_text_to_fit;
+};
+
 // https://stackoverflow.com/questions/9183050/vertical-qlabel-or-the-equivalent
-class QVLabel : public QLabel
+class QVLabel : public NewsroomLabel
 {
     Q_OBJECT
 public:
     explicit QVLabel(QWidget *parent=0);
     explicit QVLabel(const QString &text, bool configure_for_left = true, QWidget *parent=0);
+
+    void    shrink_to_fit(bool shrink = true) Q_DECL_OVERRIDE { shrink_text_to_fit = shrink; }
 
     void    set_for_left(bool for_left = true) { configure_for_left = for_left; }
 
@@ -21,4 +38,19 @@ protected:      // methods
 
 protected:      // data members
     bool    configure_for_left;
+};
+
+class QHLabel : public NewsroomLabel
+{
+    Q_OBJECT
+public:
+    explicit QHLabel(QWidget *parent=0);
+    explicit QHLabel(const QString &text, QWidget *parent=0);
+
+    void    shrink_to_fit(bool shrink = true) Q_DECL_OVERRIDE { shrink_text_to_fit = shrink; }
+
+protected:      // methods
+    void paintEvent(QPaintEvent*);
+    QSize sizeHint() const;
+    QSize minimumSizeHint() const;
 };
