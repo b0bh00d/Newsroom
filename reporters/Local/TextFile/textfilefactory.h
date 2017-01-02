7b743b5ca90d7ef8b9c8d09e99b50cbe44e41a2e
bob <bob@localhost>
2017-01-02 15:16:39 -0700

    Updated the metadata references.

--- reporters/Local/TextFile/textfilefactory.h
+++ reporters/Local/TextFile/textfilefactory.h
@@ -7,10 +7,10 @@
 class TEXTFILE_SHARED_EXPORT TextFileFactory : public IReporterFactory
 {
     Q_OBJECT
-    Q_PLUGIN_METADATA(IID "org.lucidgears.Newsroom.IPluginFactory" FILE "")
+    Q_PLUGIN_METADATA(IID "org.lucidgears.Newsroom.IReporterFactory" FILE "")
     Q_INTERFACES(IReporterFactory)
 
 public:
-    // IPluginFactory
+    // IReporterFactory
     IReporterPointer newInstance() Q_DECL_OVERRIDE;
 };
